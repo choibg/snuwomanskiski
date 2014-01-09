eee812a4cde434c582c1f46c302e65ad4527018d
choibg <choibg0417@gmail.com>
2014-01-09 12:44:52 +0900

    bg first homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snuwomanski::Application.config.secret_key_base = '72e2f8eecb35d66d84ef2ec09a158201a679305f516f88d95f5bea2c88ebf95406bc1524676b5f9991b33ac685c96b0b6fbf2d4d22e60a5e941cfd0b9d10012c'
